ab4bae8607d6e926b1419c7d4cd17e00c36deffb
MojTabaa4 <mojtabablue12@gmail.com>
2023-10-01 17:58:53 +0330

    Add Searching functionality based on author name

--- script.js
+++ script.js
@@ -1,167 +1,291 @@
 var csvFilePath = 'data/umap_visualization.csv';
+var traces;
 
 function closeInfoBox() {
     infoBox.style.display = 'none';
 }
 
-fetch(csvFilePath)
-    .then(function (response) {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.text();
-    })
-    .then(function (csvData) {
-        Papa.parse(csvData, {
-            header: true,
-            dynamicTyping: true,
-            complete: function (results) {
+function searchAuthor() {
+    var searchInput = document.getElementById("authorSearchInput").value;
+    if (searchInput.length < 3) {
+        alert("Please enter at least 3 letters to search.");
+        return;
+    }
 
-                var parsedData = results.data;
-                var traces = [];
+    var selectedDataPoints = [];
+    var customDataArray = [];
+    var tracesOfSelectedPoints = [];
 
-                for (var i = 1; i <= 10; i++) {
-                    var filteredData = parsedData.filter(function (row) {
-                        return row.cluster === i - 1;
-                    });
+    var layout = {
+        xaxis: {
+            title: '',
+            showgrid: true,
+            zeroline: false,
+            showticklabels: false,
+        },
+        yaxis: {
+            title: '',
+            showgrid: true,
+            zeroline: false,
+            showticklabels: false,
+        },
+        hoverlabel: {
+            font: {
+                size: 20,
+            },
+        },
+        legend: {
+            font: {
+                size: 20,
+            },
+        },
+        modebar: {
+            orientation: 'h',
+            bgcolor: 'black',
+            color: '#d0000b',
+            activecolor: '#fde242',
+            x: 0,
+            y: 0
+        },
+        hovermode: 'closest',
+        showlegend: false,
+        scrollZoom: true,
+        plot_bgcolor: '#FDF7F0'
+    };
 
-                    var xData = filteredData.map(function (row) {
-                        return row.x;
-                    });
-                    var yData = filteredData.map(function (row) {
-                        return row.y;
-                    });
-                    var keywordsData = filteredData.map(function (row) {
-                        return row.keywords;
-                    });
+    for (var i = 0; i < traces.length; i++) {
+        var trace = traces[i];
+        var selectedTrace = {
+            x: [],
+            y: [],
+            mode: 'markers',
+            type: 'scatter',
+            customdata: [],
+            hovertemplate: '<b>Author Name</b>: %{customdata.authorName}<br><b>Citation</b>: %{customdata.citation}<br><b>Cluster</b>: ' + (i + 1) + '<br><b>Keywords</b>: %{customdata.keywords}',
+            name: trace.name,
+            marker: {
+                size: 25,
+                colorscale: 'Jet',
+            },
+        };
 
-                    var customDataArray = filteredData.map(function (row) {
-                        return {
-                            authorName: row["Full Name"],
-                            citation: row["Citation"]
-                        };
-                    });
+        customDataArray = trace.customdata;
 
-                    var trace = {
-                        x: xData,
-                        y: yData,
-                        mode: 'markers',
-                        text: keywordsData,
-                        customdata: customDataArray,
-                        hovertemplate: 'Author Name: %{customdata.authorName}<br>Citation: %{customdata.citation}<br>Cluster: ' + i + '<br>Keywords: %{text}',
-                        marker: {
-                            size: 10,
-                            colorscale: 'Jet',
-                        },
-                        type: 'scatter',
-                        name: 'Cluster ' + i
-                    };
-                    traces.push(trace);
-                }
+        for (var j = 0; j < customDataArray.length; j++) {
+            var authorName = customDataArray[j].authorName.toLowerCase();
 
-                var layout = {
-                    xaxis: {
-                        title: '',
-                        showgrid: true,
-                        zeroline: false,
-                        showticklabels: false,
-                    },
-                    yaxis: {
-                        title: '',
-                        showgrid: true,
-                        zeroline: false,
-                        showticklabels: false,
-                    },
-                    hoverlabel: {
-                        font: {
-                            size: 15,
-                        },
-                    },
-                    legend: {
-                        font: {
-                            size: 20,
-                        },
-                    },
-                    modebar: {
-                        orientation: 'h',
-                        bgcolor: 'black',
-                        color: '#d0000b',
-                        activecolor: '#fde242',
-                        x: 0,
-                        y: 0
-                    },
-                    hovermode: 'closest',
-                    showlegend: true,
-                    scrollZoom: true,
-                    plot_bgcolor: '#FDF7F0'
+            if (authorName.includes(searchInput.toLowerCase())) {
+                var customDataArrayy = {
+                    authorName: customDataArray[j].authorName,
+                    citation: customDataArray[j].citation,
+                    keywords: trace.text[j]
                 };
 
-                Plotly.newPlot(
-                    'scatter-plot',
-                    traces,
-                    layout,
-                    {
-                        scrollZoom: true,
-                        modeBarButtonsToRemove: ['autoScale2d'],
-                        displaylogo: false,
-                        displayModeBar: true
-                    }
-                );
+                selectedDataPoints.push(j);
+                selectedTrace.x.push(trace.x[j]);
+                selectedTrace.y.push(trace.y[j]);
+                selectedTrace.customdata.push(customDataArrayy);
 
-                var modeBar = document.querySelector('.modebar-container');
-                modeBar.style.position = 'absolute';
-                modeBar.style.left = '-860px';
-                modeBar.style.top = '10px';
-
-                var plotContainer = document.getElementById('scatter-plot');
-                var clusterData = [];
-
-                traces.forEach(function (trace, i) {
-
-                    trace.customdata.forEach(function (dataObj) {
-                        dataObj.traceNumber = i;
-                    });
-                });
-
-                var infoBox = document.getElementById('infoBox');
-
-                plotContainer.on('plotly_click', function (eventData) {
-                    var pointIndex = eventData.points[0].pointIndex;
-                    var selectedData = eventData.points[0].customdata;
-                    var traceNumber = selectedData.traceNumber;
-
-                    document.getElementById('authorName').textContent = selectedData.authorName;
-                    document.getElementById('citation').textContent = selectedData.citation;
-                    document.getElementById('cluster').textContent = '' + (traceNumber + 1);
-                    document.getElementById('keywords').textContent = traces[traceNumber].text[pointIndex];
-
-                    infoBox.style.display = 'block';
-                });
-
-
-                var plotContainer = document.getElementById('scatter-plot');
-                plotContainer.on('plotly_relayout', function (eventData) {
-                    var zoomFactor = eventData['xaxis.range[1]'] - eventData['xaxis.range[0]'];
-                    console.log(zoomFactor)
-                    console.log('sdfsadffsadfsadadsfsadf')
-                    if (isNaN(zoomFactor)) {
-                        newMarkerSize = 10
-                    } else if (zoomFactor < 6) {
-                        var newMarkerSize = 50 / zoomFactor;
-                    }
-
-                    Plotly.update('scatter-plot', {
-                        'marker.size': newMarkerSize
-                    });
-                });
+                tracesOfSelectedPoints.push(selectedTrace);
             }
-            ,
-            error: function (error) {
-                console.error('Error parsing CSV:', error);
-            }
-            ,
-        });
-    })
-    .catch(function (error) {
-        console.error('There was a problem with the fetch operation:', error);
+        }
+    }
+
+    Plotly.newPlot('scatter-plot', tracesOfSelectedPoints, layout);
+
+    var modeBar = document.querySelector('.modebar-container');
+    modeBar.style.position = 'absolute';
+    modeBar.style.left = '-860px';
+    modeBar.style.top = '10px';
+
+    var plotContainer2 = document.getElementById('scatter-plot');
+    plotContainer2.on('plotly_click', function (eventData) {
+        console.log(eventData.points[0])
+        console.log('dasgsdgdakjjashahahjkhahasdfj')
+        var regex = /\d+/;
+
+        var selectedData = eventData.points[0].customdata;
+        var clusterName = eventData.points[0].data.name;
+
+        document.getElementById('authorName').textContent = selectedData.authorName;
+        document.getElementById('citation').textContent = selectedData.citation;
+        document.getElementById('cluster').textContent = clusterName.match(regex);
+        document.getElementById('keywords').textContent = selectedData.keywords;
+
+        infoBox.style.display = 'block';
     });
+
+}
+
+function resetChart() {
+    plot();
+}
+
+document.getElementById('resetButton').addEventListener('click', resetChart);
+
+function plot() {
+    fetch(csvFilePath)
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(function (csvData) {
+            Papa.parse(csvData, {
+                header: true,
+                dynamicTyping: true,
+                complete: function (results) {
+
+                    var parsedData = results.data;
+                    traces = [];
+
+                    for (var i = 1; i <= 10; i++) {
+                        var filteredData = parsedData.filter(function (row) {
+                            return row.cluster === i - 1;
+                        });
+
+                        var xData = filteredData.map(function (row) {
+                            return row.x;
+                        });
+                        var yData = filteredData.map(function (row) {
+                            return row.y;
+                        });
+                        var keywordsData = filteredData.map(function (row) {
+                            return row.keywords;
+                        });
+
+                        var customDataArray = filteredData.map(function (row) {
+                            return {
+                                authorName: row["Full Name"],
+                                citation: row["Citation"]
+                            };
+                        });
+
+                        var trace = {
+                            x: xData,
+                            y: yData,
+                            mode: 'markers',
+                            text: keywordsData,
+                            customdata: customDataArray,
+                            hovertemplate: '<b>Author Name</b>: %{customdata.authorName}<br><b>Citation</b>: %{customdata.citation}<br><b>Cluster</b>: ' + i + '<br><b>Keywords</b>: %{text}',
+                            marker: {
+                                size: 10,
+                                colorscale: 'Jet',
+                            },
+                            type: 'scatter',
+                            name: 'Cluster ' + i
+                        };
+                        traces.push(trace);
+                    }
+
+                    var layout = {
+                        xaxis: {
+                            title: '',
+                            showgrid: true,
+                            zeroline: false,
+                            showticklabels: false,
+                        },
+                        yaxis: {
+                            title: '',
+                            showgrid: true,
+                            zeroline: false,
+                            showticklabels: false,
+                        },
+                        hoverlabel: {
+                            font: {
+                                size: 15,
+                            },
+                        },
+                        legend: {
+                            font: {
+                                size: 20,
+                            },
+                        },
+                        modebar: {
+                            orientation: 'h',
+                            bgcolor: 'black',
+                            color: '#d0000b',
+                            activecolor: '#fde242',
+                            x: 0,
+                            y: 0
+                        },
+                        hovermode: 'closest',
+                        showlegend: true,
+                        scrollZoom: true,
+                        plot_bgcolor: '#FDF7F0'
+                    };
+
+                    Plotly.newPlot(
+                        'scatter-plot',
+                        traces,
+                        layout,
+                        {
+                            scrollZoom: true,
+                            modeBarButtonsToRemove: ['autoScale2d'],
+                            displaylogo: false,
+                            displayModeBar: true
+                        }
+                    );
+
+                    var modeBar = document.querySelector('.modebar-container');
+                    modeBar.style.position = 'absolute';
+                    modeBar.style.left = '-860px';
+                    modeBar.style.top = '10px';
+
+                    var plotContainer = document.getElementById('scatter-plot');
+                    var clusterData = [];
+
+                    traces.forEach(function (trace, i) {
+
+                        trace.customdata.forEach(function (dataObj) {
+                            dataObj.traceNumber = i;
+                        });
+                    });
+
+                    var infoBox = document.getElementById('infoBox');
+
+                    plotContainer.on('plotly_click', function (eventData) {
+                        console.log(eventData.points[0])
+                        console.log('dasgsdgdakjjashahahjkhahasdfj')
+                        var pointIndex = eventData.points[0].pointIndex;
+                        var selectedData = eventData.points[0].customdata;
+                        var traceNumber = selectedData.traceNumber;
+
+                        document.getElementById('authorName').textContent = selectedData.authorName;
+                        document.getElementById('citation').textContent = selectedData.citation;
+                        document.getElementById('cluster').textContent = '' + (traceNumber + 1);
+                        document.getElementById('keywords').textContent = traces[traceNumber].text[pointIndex];
+
+                        infoBox.style.display = 'block';
+                    });
+
+                    plotContainer.on('plotly_relayout', function (eventData) {
+                        var zoomFactor = eventData['xaxis.range[1]'] - eventData['xaxis.range[0]'];
+                        console.log(zoomFactor)
+                        console.log('sdfsadffsadfsadadsfsadf')
+                        if (isNaN(zoomFactor)) {
+                            newMarkerSize = 10
+                        } else if (zoomFactor < 6) {
+                            var newMarkerSize = 50 / zoomFactor;
+                        }
+
+                        Plotly.update('scatter-plot', {
+                            'marker.size': newMarkerSize
+                        });
+                    });
+                }
+                ,
+                error: function (error) {
+                    console.error('Error parsing CSV:', error);
+                }
+                ,
+            });
+        })
+        .catch(function (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+
+}
+
+plot()
