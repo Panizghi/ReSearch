acdbdb34346eee84c5f2f56f158aed7f6f20451b
paniz <panizsojaghi@gmail.com>
2024-09-23 19:52:45 -0400

    partial parse

--- acm-fellows/crawler.py
+++ acm-fellows/crawler.py
@@ -9,17 +9,25 @@ from gsc_crawler import get_google_scholar_url
 def profile_crawler(name, profile_url):
     response = requests.get(profile_url)
     soup = BeautifulSoup(response.content, 'html.parser')
-
+    
     # extract data from acm web
+        # extract data from acm web
     last_name, first_name = name.split(", ")
     full_name = soup.find('h1').text
+    if name[-1] == " ":
+        name = name[:-1:]
     awards_info = soup.find_all('section', {'class': 'awards-winners__citation'})
-    acm_award = next(award for award in awards_info if award.find('h2').a.text == 'ACM Fellows')
+    acm_award = next(award for award in awards_info if award.find('h2').a.text == 'ACM A. M. Turing Award')
     location, year = acm_award.find('h3', {'class': 'awards-winners__location'}).text.split(' - ')
-    citation = acm_award.find('p', {'class': "awards-winners__citation-short"}).text
+    citation = ' ' .join(acm_award.find('p', {'class': "awards-winners__citation-short"}).text.split('\n'))
 
     # extract gsc data
+    # try using full name & first, last name only
     gsc_data = get_google_scholar_url(full_name)
+    name_tokens = full_name.split(' ')
+    if gsc_data == {} and len(name_tokens) >= 3:
+        first_last_name = f'{name_tokens[0]} {name_tokens[-1]}'
+        gsc_data = get_google_scholar_url(first_last_name)
     if gsc_data:
         gsc_url = f'https://scholar.google.com/citations?user={gsc_data["scholar_id"]}'
         affiliation = gsc_data["affiliation"]
@@ -32,10 +40,11 @@ def profile_crawler(name, profile_url):
     return [last_name, first_name, year, location, citation, profile_url, gsc_url, affiliation, interests]
 
 # Define the URL of the page to be scraped
-url = "https://awards.acm.org/fellows/award-recipients"
+url = 'https://awards.acm.org/turing/award-recipients'
 
+headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' }
 # Send a GET request to the URL and parse the HTML content using BeautifulSoup
-response = requests.get(url)
+respo
 soup = BeautifulSoup(response.content, 'html.parser')
 
 # Find all the table rows (tr) in the table body (tbody)
@@ -48,7 +57,7 @@ rows.sort(key=lambda row: int(row.find('td', {'scope': 'row'}).text), reverse=Tr
 it = 0
 checkpoint = 'last_iteration.txt'
 
-fileName = 'acm_fellows.csv'
+fileName = 'acm_turings.csv'
 fileExist = os.path.isfile(fileName) and os.path.isfile(checkpoint)
 
 with open(fileName, 'a' if fileExist else 'w', newline='') as file:
